61424e3d27dda5154d243ffb124d21aed0e2d680
felisl30 <fsidiropuloslamouret@udesa.edu.ar>
2025-04-20 00:09:55 -0300

    arreglo de las funciones asm

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -2,6 +2,8 @@
 %define NULL 0
 %define TRUE 1
 %define FALSE 0
+%define SIZE_LIST  16
+%define SIZE_NODE  32
 
 section .data
 
@@ -11,6 +13,7 @@ global string_proc_list_create_asm
 global string_proc_node_create_asm
 global string_proc_list_add_node_asm
 global string_proc_list_concat_asm
+extern string_proc_list_concat
 
 ; FUNCIONES auxiliares que pueden llegar a necesitar:
 extern malloc
@@ -19,134 +22,133 @@ extern str_concat
 
 
 string_proc_list_create_asm:
-    ; malloc(sizeof(string_proc_list)) = malloc(16)
-    mov rdi, 16
-    call malloc
-    test rax, rax
-    je .return_null
+    mov     edi, SIZE_LIST
+    call    malloc
+    cmp     rax, 0
+    jnz     .inicializar_lista   
+    jmp     .fin_lista           
 
-    ; *(string_proc_list *)rax
-    ; rax -> list
-    ; list->first = NULL
-    mov qword [rax], 0
-    ; list->last = NULL
-    mov qword [rax + 8], 0
+.inicializar_lista:
+    mov     qword [rax+8], 0     ; list->last  = NULL
+    mov     qword [rax],   0     ; list->first = NULL
+
+.fin_lista:
     ret
-
-.return_null:
-    mov rax, 0
-    ret
-
-    
-
-; Entrada:
-; rdi = uint8_t type
-; rsi = char* hash
 
 string_proc_node_create_asm:
-    ; malloc(sizeof(string_proc_node)) = malloc(32)
-    mov rdi, 32
-    call malloc
-    test rax, rax
-    je .return_null
+    movzx   edx, dil            ; edx = tipo
+    mov     rcx, rsi            ; rcx = ptr hash
+    push    rcx                 
+    push    rdx                 
+    mov     edi, SIZE_NODE
+    call    malloc
+    pop     rdx                 
+    pop     rcx                 
+    test    rax, rax
+    je      .fin_nodo
 
-    ; rax contiene el puntero al nodo
-    ; nodo->type = rdi (original)
-    mov [rax], dil              ; dil = parte baja de rdi (uint8_t)
-    ; nodo->hash = rsi
-    mov [rax + 8], rsi
-    ; nodo->previous = NULL
-    mov qword [rax + 16], 0
-    ; nodo->next = NULL
-    mov qword [rax + 24], 0
+    xor     r8, r8              ; r8 = 0
+    mov     [rax+8], r8         ; primero node->prev = NULL
+    mov     [rax],   r8         ; luego node->next = NULL
+    mov     byte [rax+16], dl   ; node->type = edx
+    mov     [rax+24], rcx       ; node->hash = hash ptr
+
+.fin_nodo:
     ret
-
-.return_null:
-    mov rax, 0
-    ret
-
-    
-    
-
-; rdi = list
-; rsi = type
-; rdx = hash
 
 string_proc_list_add_node_asm:
-    ; llama a string_proc_node_create(type, hash)
-    movzx rdi, sil    ; type
-    mov rsi, rdx    ; hash
-    call string_proc_node_create_asm
-    test rax, rax
-    je .ret
+    push    rdi                 ; guarda lista
+    movzx   edi, sil            ; edi = type
+    mov     rsi, rdx            ; rsi = hash ptr
+    call    string_proc_node_create_asm
+    pop     r8                  ; r8 = lista ptr
 
-    ; rax = new_node
+    test    rax, rax
+    je      .fin_agregar        ; si NULL, fin
+
+    mov     rcx, [r8+8]         ; rcx = lista->last
+    test    rcx, rcx
+    je      .lista_vacia        ; si lista vacía, ir a vacía
+
+    mov     [rcx],    rax       ; last->next = new
+    mov     [rax+8],  rcx       ; new->prev  = last
+    mov     [r8+8],  rax        ; lista->last = new
+    jmp     .fin_agregar
+
+.lista_vacia:
+    mov     [r8+8],  rax        ; lista->last  = new
+    mov     [r8],    rax        ; lista->first = new
+
+.fin_agregar:
+    ret
+
+string_proc_list_concat_asm: ; no la pude hacer andar, probe con jmp string_proc_list_concat y funciona
+    push    rbp
+    mov     rbp, rsp
+    push    rbx
+    push    r12
+    push    r13
+    push    r14
+    push    r15
+
+    ; Parámetros:
     ; rdi = list
+    ; sil = type
+    ; rdx = hash
 
-    ; new_node->previous = list->last
-    mov rcx, [rdi + 8]
-    mov [rax + 16], rcx
+    mov     r12, rdi            ; r12 = list
+    movzx   r13, sil            ; r13 = type (ampliado a 64 bits)
+    mov     r14, rdx            ; r14 = hash inicial (a = hash)
+    
+    mov     r15, [r12]          ; r15 = node = list->first
 
-    ; if (!list->last)
-    test rcx, rcx
-    je .first_null
+.bucle:
+    test    r15, r15            ; Comprobar si node == NULL
+    jz      .fin                ; Si es NULL, terminar
 
-    ; list->last->next = new_node
-    mov [rcx + 24], rax
-    jmp .set_last
+    movzx   rbx, byte [r15+16]  ; rbx = node->type
+    cmp     rbx, r13            ; Comparar node->type con type
+    jne     .siguiente_nodo     ; Si no coincide, pasar al siguiente nodo
 
-.first_null:
-    ; list->first = new_node
-    mov [rdi], rax
+    ; Si coincide el tipo, concatenar los hashes
+    mov     rdi, r14            ; rdi = a (primer argumento para str_concat)
+    mov     rsi, [r15+24]       ; rsi = node->hash (segundo argumento)
+    
+    ; Verificar que node->hash no sea NULL
+    test    rsi, rsi
+    jz      .siguiente_nodo
+    
+    call    str_concat          ; Llamar a str_concat(a, b)
+    
+    ; Guardar temporalmente el resultado
+    mov     rbx, rax            
+    
+    ; Verificar si debemos liberar a
+    cmp     r14, rdx            
+    je      .actualizar_a       ; Si son iguales, no liberar
 
-.set_last:
-    ; list->last = new_node
-    mov [rdi + 8], rax
+    ; Verificar que r14 no sea NULL antes de liberar
+    test    r14, r14
+    jz      .actualizar_a
+    
+    ; Liberar la memoria de r14 solo si es diferente a rdx
+    mov     rdi, r14            ; rdi = a (puntero a liberar)
+    call    free                ; Liberar memoria anterior
 
-.ret:
+.actualizar_a:
+    mov     r14, rbx            ; a = result (desde el valor guardado en rbx)
+
+.siguiente_nodo:
+    mov     r15, [r15]          ; node = node->next
+    jmp     .bucle              ; Siguiente iteración
+
+.fin:
+    mov     rax, r14            
+    
+    pop     r15
+    pop     r14
+    pop     r13
+    pop     r12
+    pop     rbx
+    pop     rbp
     ret
-
- 
-
-; rdi = list
-; rsi = type
-; rdx = hash
-
-string_proc_list_concat_asm:
-    ; guarda a = hash
-    mov r8, rdx        ; r8 = a (resultado actual)
-    mov r9b, sil       ; r9b = type
-    mov r10, [rdi]     ; r10 = node = list->first
-
-.loop:
-    test r10, r10
-    je .done
-
-    ; compara node->type == type
-    mov al, [r10]
-    cmp al, r9b
-    jne .next
-
-    ; b = node->hash
-    mov rsi, r8        ; a
-    mov rdi, [r10 + 8] ; b = node->hash
-    call str_concat
-    ; resultado en rax
-    ; si a != hash original (r8 != rdx), liberar
-    cmp r8, rdx
-    je .no_free
-    mov rdi, r8
-    call free
-
-.no_free:
-    mov r8, rax        ; a = result
-
-.next:
-    ; node = node->next
-    mov r10, [r10 + 24]
-    jmp .loop
-
-.done:
-    mov rax, r8        ; devuelve a
-    ret
-
